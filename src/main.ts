a0818b682b1b2cdd6241bcf2b6c9626080ca8cab
MrHid3 <gkornas@proton.me>
2025-01-16 15:15:59 +0100

    chyba coś zrobiłem? jakby wywalone w to szczerze

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,65 @@
-async function requestWord() : Promise<string[]>{
-    let definition = undefined;
-    let word;
-    while(typeof(definition) == "undefined") {
-        console.log(1)
-        word = await fetch("https://random-word.ryanrk.com//api/en/word/random");
-        console.log(2)
-        word = await word.json();
+async function requestWord() : Promise<string>{
+    let word = "jaka firma?"
+    while(word.length > 7){
+        let request = await fetch("https://random-word.ryanrk.com//api/en/word/random");
+        request = await request.json();
         // @ts-ignore
-        word = await word[0].toLowerCase();
+        word = request[0].toLowerCase();
+    }
+    return word;
+}
+
+async function requestDefintion() : Promise<string[]>{
+    let definition = undefined;
+    let word = "skibidi toilet";
+    while(typeof(definition) == "undefined") {
+        word = await requestWord();
+
         let reqDef = await fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${import.meta.env.VITE_DICTIONARY_KEY}`);
         // @ts-ignore
         reqDef = await reqDef.json();
         if(typeof(await reqDef) != "undefined"){
-            // @ts-ignore
+            //@ts-ignore
             definition = await reqDef[0]["shortdef"];
-            // @ts-ignore
-            console.log(await reqDef[0]);
         }
     }
-    console.log(3);
-    console.log(word)
     return [word, definition];
 }
 
-//drugie api
-let tak = await requestWord()
-document.getElementById("app")!.innerText = await tak[0] + ", " + await tak[1];
+let big = document.querySelector(".big") as Element;
+let main = await requestWord();
+console.log(main)
+for(let i = 0; i < main.length; i++){
+    let medium = document.createElement("div");
+    medium.classList.add("medium");
+    for(let j = 0; j < 7; j++){
+        let small = document.createElement("div");
+        small.classList.add("small");
+        medium.appendChild(small);
+    }
+    big.appendChild(medium);
+}
 
-// let lista : string[] = [];
-//
-// for(let i = 0; i < main.length; i++){
-//     let is = false;
-//     while(!is) {
-//
-//         let nowe = await fetch("https://random-word.ryanrk.com//api/en/word/random")
-//         lista[i] = await nowe.json();
-//         lista[i] = lista[i][0]
-//         for (let j = 0; j < lista[i].length; j++) {
-//             if (lista[i][j] == main[i]){
-//                 is = true;
-//                 break;
-//             }
-//         }
-//     }
-//     document.getElementById("lista")!.innerText += lista[i] + " " + main[i] + "\n";
-// }
+let words = [];
+for(let i = 0; i < main.length; i++){
+    let isfine = false;
+    let word;
+    while(!isfine){
+        word = await requestDefintion();
+        for(let j = 0; j < word[0].length; j++){
+            if(main[i] == word[0][j]){
+                isfine = true;
+                break;
+            }
+        }
+    }
+    words[i] = word;
+    console.log(word);
+}
 
-//na samej górze słowo-klucz, niżej słowa które mają co najmniej jedną wspólną literkę i jaka to litera
+for(let i = 0; i < words.length; i++){
+    //@ts-ignore
+    for(let j = 6; j > words[i].length; j++){
+
+    }
+}
